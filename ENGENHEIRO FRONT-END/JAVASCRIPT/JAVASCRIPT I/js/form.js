33aba7b1801946f90dd5c2d5fab3476405c42769
Ramon <ramon_gds@hotmail.com>
2019-04-01 21:47:28 -0300

    Validação do formulário

--- ENGENHEIRO FRONT-END/JAVASCRIPT/JAVASCRIPT I/js/form.js
+++ ENGENHEIRO FRONT-END/JAVASCRIPT/JAVASCRIPT I/js/form.js
@@ -10,14 +10,32 @@ botaoAdicionar.addEventListener("click", function(event) {
     //Cria a tr e a td do paciente
     var pacienteTr = montaTr(paciente);
 
-    
+    var erros = validaPaciente(paciente);
+    if(erros.length > 0){
+        exibeMensagensDeErro(erros);
+        return; //finaliza a função aqui, acaso o paciente seja inválido
+    }
+
     //Adicionando o paciente na tabela
     var tabela = document.querySelector("#tabela-pacientes");
 
     tabela.appendChild(pacienteTr);
 
     form.reset();
+    var mensagensErro = document.querySelector("#mensagens-erro");
+    mensagensErro.innerHTML = "";
 });
+
+function exibeMensagensDeErro(erros){
+    var ul = document.querySelector("#mensagens-erro");
+    ul.innerHTML = "";
+
+    erros.forEach(function(erro){
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+    });
+}
 
 function obtemPacienteDoFormulario(form){
     var paciente = {
@@ -51,4 +69,19 @@ function montaTd(dado,classe){
     td.classList.add(classe);
 
     return td;
+}
+
+function validaPaciente(paciente) {
+
+    var erros = [];
+
+    if(!validaPeso(paciente.peso)) erros.push("Peso é inválido"); //Como é apenas uma condição, pode retirar as chaves e deixar numa só linha
+    if(!validaAltura(paciente.altura)) erros.push("Altura é inválida");
+
+    if(paciente.nome.length == 0) erros.push("O nome não pode ficar em branco");
+    if(paciente.gordura.length == 0) erros.push("A gordura não pode ficar em branco");
+    if(paciente.peso.length == 0) erros.push("O peso não pode ficar em branco");
+    if(paciente.altura.length == 0) erros.push("A altura não pode ficar em branco");
+
+    return erros;
 }
