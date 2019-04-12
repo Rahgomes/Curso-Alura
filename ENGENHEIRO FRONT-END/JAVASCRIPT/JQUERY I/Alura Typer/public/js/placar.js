c80cdb356d7cbec41cdf3fbf188424c816a5096f
Ramon <ramon_gds@hotmail.com>
2019-04-12 11:16:54 -0300

    Enviando dados com POST

--- ENGENHEIRO FRONT-END/JAVASCRIPT/JQUERY I/Alura Typer/public/js/placar.js
+++ ENGENHEIRO FRONT-END/JAVASCRIPT/JQUERY I/Alura Typer/public/js/placar.js
@@ -1,3 +1,6 @@
+$("#botao-placar").click(mostraPlacar);
+$("#botao-sync").click(sincronizaPlacar);
+
 function inserePlacar() {
     var corpoTabela = $(".placar").find("tbody");
     var usuario = "Douglas"
@@ -21,11 +24,44 @@ function scrollPlacar() {
     }, 1000);
 }
 
-$("#botao-placar").click(mostraPlacar);
-
 function mostraPlacar() {
     $(".placar").stop().slideToggle(600);
 }
+
+function sincronizaPlacar(){
+    var placar = [];
+    var linhas = $("tbody>tr");
+    linhas.each(function(){
+        var usuario = $(this).find("td:nth-child(1)").text();
+        var palavras = $(this).find("td:nth-child(2)").text();
+
+        var score = {
+            usuario: usuario,
+            pontos: palavras
+        };
+
+        placar.push(score);
+    });
+
+    var dados = {
+        placar: placar
+    };
+    
+    $.post("http://localhost:3000/placar",dados,function(){
+        console.log("Salvou o placar")
+    });
+}
+
+function atualizaPlacar(){
+    $.get("http://localhost:3000/placar", function(data){
+        $(data).each(function(){
+            var linha = novaLinha(this.usuario, this.pontos);
+            linha.find(".botao-remover").click(removeLinha);
+            $("tbody").append(linha);
+        });
+    });
+}
+
 
 function novaLinha(usuario, palavras) {
     var linha = $("<tr>");
